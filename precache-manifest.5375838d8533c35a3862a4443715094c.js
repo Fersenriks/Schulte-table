b367ab576b3e322a2ecd333da85f9a5e799c3cf2
VladimirIvanov <vladimir.ivanov1022@gmail.com>
2021-01-24 19:47:47 +0200

    Updates

---
+++ precache-manifest.5375838d8533c35a3862a4443715094c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ada01b36b6043158e2e1fafbebe202b",
+    "url": "/Schulte-table/index.html"
+  },
+  {
+    "revision": "dca31ad387c91c3757ab",
+    "url": "/Schulte-table/static/css/main.1d65bb56.chunk.css"
+  },
+  {
+    "revision": "1e4f5fb4d1783013e362",
+    "url": "/Schulte-table/static/js/2.013f5dba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Schulte-table/static/js/2.013f5dba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dca31ad387c91c3757ab",
+    "url": "/Schulte-table/static/js/main.dfd6432f.chunk.js"
+  },
+  {
+    "revision": "ca88e41077428efea51c",
+    "url": "/Schulte-table/static/js/runtime-main.ff5b3b47.js"
+  },
+  {
+    "revision": "e0cc50d0c0fe5d82de184a903aa8d461",
+    "url": "/Schulte-table/static/media/cat.e0cc50d0.jpg"
+  }
+]);
